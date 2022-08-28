15b1957cbf4dc7f22e55afe777a0594aba106ac5
e.vrailas <e.vrailas@pmmretail.com>
2022-08-28 16:48:08 +0300

    Add findBySerial function

--- src/main/java/com/team3/fooddeliverybackend/repository/ProductRepository.java
+++ src/main/java/com/team3/fooddeliverybackend/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.team3.fooddeliverybackend.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findBySerial(String serial);
 }
