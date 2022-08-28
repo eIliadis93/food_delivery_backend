75e399fd6edc4b36b3aa5d5537cdbede61ed4a02
e.vrailas <e.vrailas@pmmretail.com>
2022-08-28 16:51:24 +0300

    Add findBySerial function

--- src/main/java/com/team3/fooddeliverybackend/service/ProductService.java
+++ src/main/java/com/team3/fooddeliverybackend/service/ProductService.java
@@ -3,4 +3,5 @@ package com.team3.fooddeliverybackend.service;
 import com.team3.fooddeliverybackend.domain.Product;
 
 public interface ProductService extends BaseService<Product>{
+    Product findBySerial(String serial);
 }
