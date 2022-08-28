51a66f833cb66c59e2c229f7907a2141f45a36bb
e.iliadis <e.iliadis@pmmretail.com>
2022-08-28 14:11:21 +0300

    Create the project.

---
+++ src/test/java/com/team3/fooddeliverybackend/FoodDeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.fooddeliverybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
