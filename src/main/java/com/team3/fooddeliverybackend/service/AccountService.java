df9294827ca1e83395d2dfa949a9cebb28a379a2
s.christodoulou <s.christodoulou@pmmretail.com>
2022-08-30 10:45:05 +0300

    Modify AccountService

--- src/main/java/com/team3/fooddeliverybackend/service/AccountService.java
+++ src/main/java/com/team3/fooddeliverybackend/service/AccountService.java
@@ -3,4 +3,5 @@ package com.team3.fooddeliverybackend.service;
 import com.team3.fooddeliverybackend.domain.Account;
 
 public interface AccountService extends BaseService<Account>{
+    Account findByEmail(String email);
 }
