db6ac20359e1b3e4985abcd7406961346ca154a9
e.iliadis <e.iliadis@pmmretail.com>
2022-09-06 17:21:01 +0300

    Add transfer package (ApiError , ApiResponse) Fix StoreController findByName

--- src/main/java/com/team3/fooddeliverybackend/controller/StoreController.java
+++ src/main/java/com/team3/fooddeliverybackend/controller/StoreController.java
@@ -4,10 +4,15 @@ import com.team3.fooddeliverybackend.domain.Store;
 import com.team3.fooddeliverybackend.domain.StoreCategory;
 import com.team3.fooddeliverybackend.service.BaseService;
 import com.team3.fooddeliverybackend.service.StoreService;
+import com.team3.fooddeliverybackend.transfer.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -22,22 +27,22 @@ public class StoreController extends BaseController<Store> {
         return storeService;
     }
 
-    @GetMapping(params = "{name}")
-    public ResponseEntity<Store> findByName(@PathVariable @RequestParam String name) {
+    @GetMapping(params = "name")
+    public ResponseEntity<ApiResponse<Store>> findByName(@RequestParam(value = "name") String name) {
         final Store byName = storeService.findByName(name);
         if (byName == null) {
             throw new NoSuchElementException("Store not found");
         }
-        return ResponseEntity.ok(byName);
+        return ResponseEntity.ok(ApiResponse.<Store>builder().data(byName).build());
     }
 
     @GetMapping(params = "storeByCategory")
-    public ResponseEntity<Store> findByStoreCategory(@RequestParam StoreCategory storeCategory) {
-        final Store byCategory = (Store) storeService.findByStoreCategory(storeCategory);
+    public ResponseEntity<ApiResponse<List<Store>>> findByStoreCategory(@RequestParam StoreCategory storeCategory) {
+        final List<Store> byCategory = storeService.findByStoreCategory(storeCategory);
         if (byCategory == null) {
             throw new NoSuchElementException("Store not found (by category)");
         }
-        return ResponseEntity.ok(byCategory);
+        return ResponseEntity.ok(ApiResponse.<List<Store>>builder().data(byCategory).build());
 
     }
 }
