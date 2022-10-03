b3c4d9bf0c9fc026f2170b15df9b055102868852
e.iliadis <e.iliadis@pmmretail.com>
2022-10-03 20:38:58 +0300

    Added products data

--- src/main/java/com/team3/fooddeliverybackend/domain/ProductCategory.java
+++ src/main/java/com/team3/fooddeliverybackend/domain/ProductCategory.java
@@ -1,5 +1,5 @@
 package com.team3.fooddeliverybackend.domain;
 
 public enum ProductCategory {
-    FOOD,COFFEE,DRINK,DESERT
+    FOOD,COFFEE,DRINK,DESERT,BURGER,MENU,SALAD,BAO,TYLIXTA,PIZZA,VEGETABLES,FRUITS,CREPE
 }
